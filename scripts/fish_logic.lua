e5381eb88ea65fa9b1eb56c46bbb2d5db8e7c5fb
shonicth <shonicth@gmail.com>
2026-04-30 12:26:57 -0400

    Initial fishsanity rework push
    this might break a lot. Currently supports only Limsa

---
+++ scripts/fish_logic.lua
@@ -0,0 +1,76 @@
+logicFishList = {
+    --Limsa Lower Decks
+    ["MerlthorGobyLowerDecks"] = {"lugworm"},
+    ["LominsaAnchovyLowerDecks"] = {"lugworm"},
+    ["MalmKelpLowerDecks"] = {"lugworm", "pillbug"},
+    ["FingerShrimpLowerDecks"] = {"lugworm"},
+    ["OceanCloudLowerDecks"] = {"pillbug"},
+    ["HarborHerringLowerDecks"] = {"pillbug"},
+    ["CoralButterflyLowerDecks"] = {"pillbug", "floatingminnow"},
+    ["BlueOctopusLowerDecks"] = {"lugworm", "pillbug"},
+    ["OctomammothLowerDecks"] = {"pillbug"},
+
+    --Limsa Upper Decks
+    ["MerlthorGobyUpperDecks"] = {"lugworm"},
+    ["LominsanAnchovyUpperDecks"] = {"lugworm"},
+    ["MalmKelpUpperDecks"] = {"lugworm", "pillbug"},
+    ["FingerShrimpUpperDecks"] = {"lugworm"},
+    ["OceanCloudUpperDecks"] = {"pillbug"},
+    ["HarborHerringUpperDecks"] = {"pillbug"},
+    ["CoralButterflyUpperDecks"] = {"pillbug", "floatingminnow"},
+    ["BlueOctopusUpperDecks"] = {"lugworm", "pillbug"},
+    ["GoldenfinUpperDecks"] = {"pillbug"}
+
+    --Middle La Noscea Zephyr Drift
+
+    --Middle La Noscea Rouge River
+
+    --Middle La Noscea West Agelyss River
+
+    --Middle La Noscea Summerford
+
+    --Middle La Noscea Nym River
+
+    --Middle La Noscea Woad Whisper Canyon
+
+},
+
+
+
+outLogicFishList = {
+    --Limsa Lower Decks
+    ["MerlthorGobyLowerDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["LominsaAnchovyLowerDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["MalmKelpLowerDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["FingerShrimpLowerDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["OceanCloudLowerDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["HarborHerringLowerDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["CoralButterflyLowerDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["BlueOctopusLowerDecks"] = {table.unpack(outLogicFishList["HarborHerringLowerDecks"]), table.unpack(outLogicFishList["MerlthorGobyLowerDecks"]), table.unpack(outLogicFishList["OceanCloudLowerDecks"])},
+    ["OctomammothLowerDecks"] = {table.unpack(outLogicFishList["HarborHerringLowerDecks"]), table.unpack(outLogicFishList["MerlthorGobyLowerDecks"])}
+
+    --Limsa Upper Decks
+    ["MerlthorGobyUpperDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["LominsanAnchovyUpperDeks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["MalmKelpUpperDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["FingerShrimpUpperDecks"] = {"crabball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "lugworm", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["OceanCloudUpperDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["HarborHerringUpperDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["CoralButterflyUpperDecks"] = {"crabball", "crayfishball", "floatingminnow", "glowworm", "gobyball", "heavysteeljig", "herringball", "krillcagefeeder", "northernkrill", "pillbug", "rattail", "saltwaterboilie", "shrimpcagefeeder", "sinkingminnow", "spoonworm", "steeljig", "versatilelure", "yumizuno"},
+    ["BlueOctopusUpperDecks"] = {table.unpack(outLogicFishList["HarborHerringLowerDecks"]), table.unpack(outLogicFishList["MerlthorGobyUpperDecks"]), table.unpack(outLogicFishList["OceanCloudUpperDecks"])},
+    ["GoldenfinUpperDecks"] = {"pillbug"}
+
+    
+    --Middle La Noscea Zephyr Drift
+
+    --Middle La Noscea Rouge River
+
+    --Middle La Noscea West Agelyss River
+
+    --Middle La Noscea Summerford
+
+    --Middle La Noscea Nym River
+
+    --Middle La Noscea Woad Whisper Canyon
+    
+}
--- scripts/logic.lua
+++ scripts/logic.lua
@@ -357,3 +357,27 @@ function leveling()
     math.max(Tracker:ProviderCountForCode("5pldlevels"), Tracker:ProviderCountForCode("5warlevels"), Tracker:ProviderCountForCode("5drklevels"), Tracker:ProviderCountForCode("5gnblevels"), Tracker:ProviderCountForCode("5whmlevels"), Tracker:ProviderCountForCode("5schlevels"), Tracker:ProviderCountForCode("5astlevels"), Tracker:ProviderCountForCode("5sgelevels"), Tracker:ProviderCountForCode("5mnklevels"), Tracker:ProviderCountForCode("5drglevels"), Tracker:ProviderCountForCode("5ninlevels"), Tracker:ProviderCountForCode("5samlevels"), Tracker:ProviderCountForCode("5rprlevels"), Tracker:ProviderCountForCode("5vprlevels"), Tracker:ProviderCountForCode("5brdlevels"), Tracker:ProviderCountForCode("5mchlevels"), Tracker:ProviderCountForCode("5dnclevels"), Tracker:ProviderCountForCode("5blmlevels"), Tracker:ProviderCountForCode("5smnlevels"), Tracker:ProviderCountForCode("5rdmlevels"), Tracker:ProviderCountForCode("5pctlevels"), Tracker:ProviderCountForCode("5blulevels"))
   )
 end
+
+function FishAccess(location, reqFshLevel)
+  local fshLevels = Tracker:ProviderCountForCode("fshlevels")
+  if fshLevels < reqFshLevel then
+    return AccessibilityLevel.None
+  end
+  logicFish = logicFishList[location]
+  outLogicFish = outLogicFishList[location]
+  if logicFish ~= nil then
+    for _, item in pairs(logicFish) do
+      if Tracker:ProviderCountForCode(item) > 0 then
+        return AccessibilityLevel.Normal
+      end
+    end
+  end
+  if outLogicFish ~= nil then
+    for _, item in pairs(outLogicFish) do
+      if Tracker:ProviderCountForCode(item) > 0 then
+        return AccessibilityLevel.SequenceBreak
+      end
+    end
+  end
+  return AccessibilityLevel.None
+end
